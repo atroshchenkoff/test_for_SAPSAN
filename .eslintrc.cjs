cd2b7d2b96f6f4a5468641a960a75184c9c4d8f6
Fedor A <fodor2994@mail.ru>
2024-04-24 13:33:56 +0300

    feat: project configure

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'es5',
+        quoteProps: 'preserve'
+      }
+    ],
+    quotes: [
+      2,
+      'single',
+      {
+        allowTemplateLiterals: true,
+        avoidEscape: true
+      }
+    ]
+  },
+}
